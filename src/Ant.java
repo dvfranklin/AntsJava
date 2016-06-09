1dcb212c547f1be824a7ef925ccedc5789e3fa8a
Dan <dvfranklin@gmail.com>
2016-06-09 17:30:59 -0400

    Initial commit

---
+++ src/Ant.java
@@ -0,0 +1,13 @@
+import javafx.scene.paint.Color;
+
+public class Ant {
+    double x;
+    double y;
+
+    Color color = Color.BLACK;
+
+    public Ant(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,128 @@
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+import static javafx.scene.paint.Color.RED;
+
+public class Main extends Application {
+    static final int WIDTH = 800;
+    static final int HEIGHT = 600;
+
+    static final int ANT_COUNT = 100;
+
+    ArrayList<Ant> ants;
+
+    long lastTimestamp = 0;
+
+    int fps(long now) {
+        double diff = now - lastTimestamp;
+        double diffSeconds = diff / 1000000000;
+        return (int) (1 / diffSeconds);
+    }
+
+    static ArrayList<Ant> createAnts() {
+        ArrayList<Ant> ants = new ArrayList<>();
+        for (int i = 0; i < ANT_COUNT; i++) {
+            Random r = new Random();
+            ants.add(new Ant(r.nextInt(WIDTH), r.nextInt(HEIGHT)));
+        }
+        return ants;
+    }
+
+    void drawAnts(GraphicsContext context) {
+        context.clearRect(0, 0, WIDTH, HEIGHT);
+        for (Ant ant : ants) {
+            context.setFill(ant.color);
+            context.fillOval(ant.x, ant.y, 5, 5);
+        }
+    }
+
+    Ant aggravateAnt(Ant testAnt){
+        try {
+            Thread.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        ArrayList<Ant> closeAnts = new ArrayList<>();
+        for(int i = 0; i < ANT_COUNT; i++){
+            if(Math.abs(testAnt.x - ants.get(i).x) < 5 || Math.abs(testAnt.y - ants.get(i).y) < 5){
+                closeAnts.add(ants.get(i));
+            }
+        }
+        if(closeAnts.size() > 1){
+            testAnt.color = Color.RED;
+        } else {
+            testAnt.color = Color.BLACK;
+        }
+        return testAnt;
+    }
+
+    static double randomStep() {
+        return Math.random() * 2 - 1;
+    }
+
+    Ant moveAnt(Ant ant) {
+        /*try {
+            Thread.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
+
+        ant.x += randomStep();
+        ant.y += randomStep();
+        return ant;
+    }
+
+    void moveAnts() {
+        ants = ants.parallelStream()
+                .map(this::aggravateAnt)
+                .map(this::moveAnt)
+                .collect(Collectors.toCollection(ArrayList<Ant>::new));
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        Scene scene = new Scene(root, WIDTH, HEIGHT);
+
+        primaryStage.setTitle("Ants");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        Label fpsLabel = (Label) scene.lookup("#fps");
+
+        Canvas canvas = (Canvas) scene.lookup("#canvas");
+        GraphicsContext context = canvas.getGraphicsContext2D();
+
+        ants = createAnts();
+
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                moveAnts();
+                drawAnts(context);
+                fpsLabel.setText(fps(now) + "");
+                lastTimestamp = now;
+            }
+        };
+        timer.start();
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
